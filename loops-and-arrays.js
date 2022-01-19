4c179d7585ba3f0b2577497d700b7842221a03b7
JasperHX <jasper-hendrikx@hotmail.com>
2022-01-19 22:03:17 +0100

    huiswerk klaar

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -37,8 +37,15 @@ const numbers = [2, 4, 5, 29, 38];
 console.log(numbers);
 
 for (let i = 0; i < numbers.length; i++) {
-    numbers[i] = numbers[i] * 2
-};
+
+    if (i % 2 === 0) {
+        numbers[i] = numbers[i] * 2;
+    } else {
+        numbers[i] = numbers[i] * 3;
+    }
+}
+console.log(numbers);
+
 
 // ==========================================
 // Opdracht 3
@@ -65,6 +72,6 @@ const squares = [30, 2, 8, 24, 11];
 console.log(squares)
 
 for (let i = 0; i < squares.length; i++) {
-    squares[i] = squares[i] * squares[i] * squares[i]
+    squares[i] = 'Het volume van ' + squares[i] + ' is ' + squares[i] * squares[i] * squares[i];
 }
 console.log(squares)
